814dc0afba0f5b309415ac0c3bc0980ce41f8abf
Arkadjio <redwhitep@gmail.com>
2024-10-25 15:24:54 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,19 @@
+from True_math import divide as True_dvd_1
+from Fake_math import divide as Fake_dvd
+
+
+result1 = Fake_dvd(69, 3)
+
+result2 = Fake_dvd(3, 0)
+
+result3 = True_dvd_1(49, 7)
+
+result4 = True_dvd_1(15, 0)
+
+print(result1)
+
+print(result2)
+
+print(result3)
+
+print(result4)
